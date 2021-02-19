7053cee3e10d2cc5b4adfe92c90db6b0233be662
Yael Monserrat Alba Muñoz <74206468+MonseAlba7@users.noreply.github.com>
2021-02-19 08:23:53 -0600

    Add files via upload

---
+++ CicloFor/src/ciclofor/CicloFor.java
@@ -0,0 +1,17 @@
+package ciclofor;
+/**
+ * @author Monse Alba
+ */
+public class CicloFor {
+
+    public static void main(String[] args) {
+  
+        for(int contador=1;contador<=20;contador++)
+        {
+            contador=contador*2;
+            System.out.println("El contador vale: "+contador);
+        }
+        
+    }
+    
+}
