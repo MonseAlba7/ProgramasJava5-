45179114899366068aeb27dff87262d812e8f977
Yael Monserrat Alba Muñoz <74206468+MonseAlba7@users.noreply.github.com>
2021-02-19 08:26:33 -0600

    Add files via upload

---
+++ VariablesyOperacionesAritmeticas/src/variablesyoperacionesaritmeticas/VariablesyOperacionesAritmeticas.java
@@ -0,0 +1,23 @@
+package variablesyoperacionesaritmeticas;
+/**
+ * @author Monse Alba 
+ */
+public class VariablesyOperacionesAritmeticas {
+
+    public static void main(String[] args) {
+        short suma;
+        suma=3+10;
+        int resta=8-17;
+        long residuo=9/2;
+        float multiplicacion=2*(15*(-2));
+        double division=10/3.4;
+        System.out.println("el resultado de la suma es: "+suma);
+        System.out.println("el resultado de la resta es: "+resta);
+        System.out.println("el resultado de la multiplicacion es: "+multiplicacion);
+        System.out.println("el resultado de la division es: "+division);
+        System.out.println("el residuo de la division es: "+residuo);
+    
+    
+    }
+    
+}
