9629be27d3a6faecaa9d86bc99b57795e56457e5
Yael Monserrat Alba Muñoz <74206468+MonseAlba7@users.noreply.github.com>
2021-02-19 08:25:18 -0600

    Add files via upload

---
+++ InstruccionWhile/src/instruccionwhile/InstruccionWhile.java
@@ -0,0 +1,19 @@
+package instruccionwhile;
+/**
+ * @author Monse Alba
+ */
+public class InstruccionWhile {
+
+    public static void main(String[] args) {
+        boolean cond=true;
+        int x=0;
+        while(x<=15)
+        {
+            System.out.println("El valor de x es: "+x);
+            x=(x*2)+3;    
+            
+        }
+       
+    }
+    
+}
