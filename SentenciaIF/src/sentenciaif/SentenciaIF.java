477193550d24c611bf1dcdf75e7b91d2512ccb7f
Yael Monserrat Alba Muñoz <74206468+MonseAlba7@users.noreply.github.com>
2021-02-19 08:25:39 -0600

    Add files via upload

---
+++ SentenciaIF/src/sentenciaif/SentenciaIF.java
@@ -0,0 +1,22 @@
+package sentenciaif;
+/**
+ * @author Monse Alba
+ */
+public class SentenciaIF {
+    public static void main(String[] args) {
+        int var=6;
+        int var2=10;
+           
+        if(!((var==6) || (var2>5)))
+        {
+           System.out.println("Tu operacion es verdadera");
+        }
+        else
+        {
+            System.out.println("Tu operacion es falsa");
+        }
+        
+        
+    }
+    
+}
